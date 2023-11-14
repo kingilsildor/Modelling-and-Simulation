a67abe408d67342d65f791ac5e38964313dbcc34
kingilsildor <tychostam@gmail.com>
2023-11-14 14:15:39 +0100

    Added the function to simulate car density

--- code/car_density_gui.py
+++ code/car_density_gui.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from pyics import Model
 
 def decimal_to_base_k(n, k):
@@ -22,12 +23,13 @@ class CASim(Model):
 
         self.t = 0
         self.config = None
-        self.car_flow = []
+        self.car_flow = 0
+        self.car_dict = {}
 
         self.make_param('r', 1)
         self.make_param('k', 2)
         self.make_param('width', 50)
-        self.make_param('height', 50)
+        self.make_param('height', 1000)
         self.make_param('rule', 184, setter=self.setter_rule)        
         self.make_param('density', 0.40)        
 
@@ -77,6 +79,7 @@ class CASim(Model):
         """Initializes the configuration of the cells and converts the entered
         rule number to a rule set."""
         self.t = 0
+        self.car_flow = 0
         self.config = np.zeros([self.height, self.width])
         self.config[0, :] = self.setup_initial_row()
         self.build_rule_set()  
@@ -99,7 +102,6 @@ class CASim(Model):
         row) and applying the rule to determine the state of the cells."""
         self.t += 1
         if self.t >= self.height:
-            self.car_flow_df()
             return True     
 
         for patch in range(self.width):
@@ -112,17 +114,30 @@ class CASim(Model):
             values = self.config[self.t - 1, indices]
             self.config[self.t, patch] = self.check_rule(values)
             
-            if patch == 49:
-                self.car_flow.append(self.check_rule(values))
-    
-    def car_flow_df(self):
         """ Calculate the amount of cars that cross the right-hand side
-        system boundary per unit of time."""
-        print(sum(self.car_flow)) 
-            
+        system boundary per unit of time."""    
+        if (self.config[self.t, 0] == 0 and self.config[self.t, self.width - 1] == 1):
+            self.car_flow += 1
+
+
     
+def run_simulation_for_density(sim, density, N=50, T=1000):
+    sim.density = density
+    sim.width = N
+    sim.reset()
+
+    for _ in range(T):
+        sim.step()
+
+    print(f"Density: {round(density, 2)}, Car Flow: {sim.car_flow}")
+
 if __name__ == '__main__':
-    sim = CASim()    
-    from pyics import GUI
-    cx = GUI(sim)
-    cx.start()
+    sim = CASim()
+    densities = np.arange(0, 1.05, 0.05)
+
+    for density in densities:        
+        run_simulation_for_density(sim, density)
+
+# from pyics import GUI
+# cx = GUI(sim)
+# cx.start()
