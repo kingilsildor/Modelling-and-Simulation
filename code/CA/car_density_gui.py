de31e9809d082bdee5add45fca6af5337f2fa54b
kingilsildor <tychostam@gmail.com>
2023-11-20 15:07:26 +0100

    Changed file structure

--- code/CA/car_density_gui.py
+++ code/CA/car_density_gui.py
@@ -7,7 +7,7 @@ def decimal_to_base_k(n, k):
     """Converts a given decimal (i.e. base-10 integer) to a list containing the
     base-k equivalant.
 
-    For example, for n=34 and k=3 this function should return [1, 0, 2, 1]."""    
+    For example, for n=34 and k=3 this function should return [1, 0, 2, 1]."""
     if n == 0:
         return [0]
     
@@ -27,12 +27,12 @@ class CASim(Model):
 
         self.make_param('r', 1)
         self.make_param('k', 2)
-        self.make_param('rule', 184)        
+        self.make_param('rule', 184)
      
 
     def setter_rule(self, val):
         """Setter for the rule parameter, clipping its value between 0 and the
-        maximum possible rule number."""        
+        maximum possible rule number."""
         self.rule_set_size = self.k ** (2 * self.r + 1)
         self.max_rule_number = self.k ** self.rule_set_size
         return max(0, min(val, self.max_rule_number - 1))
@@ -43,7 +43,7 @@ class CASim(Model):
 
         For example, for rule=34, k=3, r=1 this function should set rule_set to
         [0, ..., 0, 1, 0, 2, 1] (length 27). This means that for example
-        [2, 2, 2] -> 0 and [0, 0, 1] -> 2."""        
+        [2, 2, 2] -> 0 and [0, 0, 1] -> 2."""
         base_arr = decimal_to_base_k(self.rule, self.k)
         rule_arr = np.zeros(self.rule_set_size)
         rule_arr[-len(base_arr):] = base_arr
@@ -53,7 +53,7 @@ class CASim(Model):
         """Returns the new state based on the input states.
 
         The input state will be an array of 2r+1 items between 0 and k, the
-        neighbourhood which the state of the new cell depends on."""        
+        neighbourhood which the state of the new cell depends on."""
         reversed_ruleset = self.rule_set[::-1]
         base_index = 0
 
@@ -63,17 +63,17 @@ class CASim(Model):
 
     def setup_initial_row(self, density):
         """Returns an array of length `width' with the initial state for each of
-        the cells in the first row. Values should be between 0 and k."""        
+        the cells in the first row. Values should be between 0 and k."""
         initial = None
         if ( 0 <= density <= 1):
-            initial = np.random.choice(self.k, size=self.N, p=[1 - density, density])        
+            initial = np.random.choice(self.k, size=self.N, p=[1 - density, density])
         else:
             np.random.seed(density)
             initial = np.random.randint(0, self.k, size=self.N)
         return initial
                   
     def make_new_gen(self, row):
-        """ Make new gen, makes a new generation of the input row according to the build ruleset"""
+        """Make new gen, makes a new generation of the input row according to the build ruleset"""
         new_gen_row = []
         length_row = len(row)
         
@@ -162,7 +162,7 @@ def estimation_graph(repeat,  N=50, T=1000):
 =======
         for num in range(length_row):
             if num == 0:
-                # Infer the first element in the row. 
+                # Infer the first element in the row.
                 current_row = list(row[length_row-self.r:]) + list(row[:self.r+1])
             elif num == length_row-1:
                 # Infer the last element in the row.
@@ -179,7 +179,7 @@ def estimation_graph(repeat,  N=50, T=1000):
     
     def run_simulation_for_density(self, density, sim_amount=30, N=50, T=1000):
         """Runs sim_amount simulations with specified density on a traffic simulation object
-        for a given time (T) and road length (N), returning the rounded average resulting car 
+        for a given time (T) and road length (N), returning the rounded average resulting car
         flow of previous mentioned simulations."""
         self.N = N
         self.setter_rule(self.rule)
@@ -208,7 +208,7 @@ def estimation_graph(repeat,  N=50, T=1000):
         densities = [x/100 for x in range(0,105, 5)]
 
         df = pd.DataFrame(columns=['density', 'car flow'])
-        for density in densities:        
+        for density in densities:
             df.loc[len(df)] = [round(density, 2), self.run_simulation_for_density(density=density, N=N, T=T)]
         
 
@@ -224,7 +224,8 @@ def estimation_graph(repeat,  N=50, T=1000):
     def estimation_graph(self, repeat, N=50, T=1000):
         """ First calculates different time ranges (t), repeat amount of times for 21 different densities. 
         For each time range the critical density is calculated and compared to this density the correctness probability is calculated.
-        In the end a plot is made for all time ranges and their corresponding probability of correctness"""
+        In the end a plot is made for all time ranges and their corresponding probability of correctness.
+        This function can take some time to load."""
         densities = [x/100 for x in range(0,105, 5)]
         t_ranges = [count for count in range(10, 60)]
         group = 0
@@ -233,10 +234,10 @@ def estimation_graph(repeat,  N=50, T=1000):
 
         for t in t_ranges:
             for _ in range(repeat):
-                for density in densities:        
+                for density in densities:
                     df.loc[len(df)] = [group, round(density, 2), self.run_simulation_for_density(density=density, N=N, T=t), t]
                 group +=1
-            print(f"T-range: {t}, is done! (last one is {t_ranges[-1]})")  
+            print(f"T-range: {t}, is done! (last one is {t_ranges[-1]})")
 
         result = pd.DataFrame(columns=['time steps', 'probability correct'])
         
@@ -248,7 +249,7 @@ def estimation_graph(repeat,  N=50, T=1000):
             
             # Critical density is choses by chosing the highest measured car flow and the corresponding density
             critical_density = values.loc[values['car flow'].idxmax(), 'density']
-            crit_min, crit_max = critical_density-0.05, critical_density+0.05
+            crit_min, crit_max = critical_density - 0.05, critical_density + 0.05
 
             # Beneath calculates the probability of densities being in range of the critical density
             count = 0
@@ -266,7 +267,8 @@ def estimation_graph(repeat,  N=50, T=1000):
             yaxis_title='Probability Correct')
 
         fig.update_traces(marker={'size': 15})
-        fig.show()   
+        fig.update_layout(font=dict(size=20))
+        fig.show()
             
 
 <<<<<<< HEAD
@@ -289,8 +291,8 @@ if __name__ == '__main__':
 =======
 if __name__ == '__main__':
     sim = CASim()
-    sim.plot_df(N=3, T=5)
-    sim.plot_df()
+    # sim.plot_df(N=3, T=5)
+    # sim.plot_df()
     sim.estimation_graph(20)
 >>>>>>> 8f6aea8 (Nu werkt alles! Tijd voor weekend!)
 
---
+++ code/malaria_skeleton/malaria_skeleton/malaria_skeleton.py
@@ -0,0 +1,202 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import malaria_visualize
+
+
+class Model:
+    def __init__(self, width=50, height=50, nHuman=10, nMosquito=20,
+                 initMosquitoHungry=0.5, initHumanInfected=0.2,
+                 humanInfectionProb=0.25, mosquitoInfectionProb=0.9,
+                 biteProb=1.0):
+        """
+        Model parameters
+        Initialize the model with the width and height parameters.
+        """
+        self.height = height
+        self.width = width
+        self.nHuman = nHuman
+        self.nMosquito = nMosquito
+        self.humanInfectionProb = humanInfectionProb
+        self.mosquitoInfectionProb = mosquitoInfectionProb
+        self.biteProb = biteProb
+        # etc.
+
+        """
+        Data parameters
+        To record the evolution of the model
+        """
+        self.infectedCount = 0
+        self.deathCount = 0
+        # etc.
+
+        """
+        Population setters
+        Make a data structure in this case a list with the humans and mosquitos.
+        """
+        self.humanPopulation = self.set_human_population(initHumanInfected)
+        self.mosquitoPopulation = self.set_mosquito_population(initMosquitoHungry)
+
+    def set_human_population(self, initHumanInfected):
+        """
+        This function makes the initial human population, by iteratively adding
+        an object of the Human class to the humanPopulation list.
+        The position of each Human object is randomized. A number of Human
+        objects is initialized with the "infected" state.
+        """
+        humanPopulation = []
+        for i in range(self.nHuman):
+            x = np.random.randint(self.width)
+            y = np.random.randint(self.height)
+            """
+            To implement: Humans may not have overlapping positions.
+            """
+            if (i / self.nHuman) <= initHumanInfected:
+                state = 'I'  # I for infected
+            else:
+                state = 'S'  # S for susceptible
+            humanPopulation.append(Human(x, y, state))
+        return humanPopulation
+
+    def set_mosquito_population(self, initMosquitoHungry):
+        """
+        This function makes the initial mosquito population, by iteratively
+        adding an object of the Mosquito class to the mosquitoPopulation list.
+        The position of each Mosquito object is randomized.
+        A number of Mosquito objects is initialized with the "hungry" state.
+        """
+        mosquitoPopulation = []
+        for i in range(self.nMosquito):
+            x = np.random.randint(self.width)
+            y = np.random.randint(self.height)
+            if (i / self.nMosquito) <= initMosquitoHungry:
+                hungry = True
+            else:
+                hungry = False
+            mosquitoPopulation.append(Mosquito(x, y, hungry))
+        return mosquitoPopulation
+
+    def update(self):
+        """
+        Perform one timestep:
+        1.  Update mosquito population. Move the mosquitos. If a mosquito is
+            hungry it can bite a human with a probability biteProb.
+            Update the hungry state of the mosquitos.
+        2.  Update the human population. If a human dies remove it from the
+            population, and add a replacement human.
+        """
+        for i, m in enumerate(self.mosquitoPopulation):
+            m.move(self.height, self.width)
+            for h in self.humanPopulation:
+                if m.position == h.position and m.hungry\
+                   and np.random.uniform() <= self.biteProb:
+                    m.bite(h, self.humanInfectionProb,
+                           self.mosquitoInfectionProb)
+        """
+        To implement: set the hungry state from false to true after a
+                     number of time steps has passed.
+        """
+
+        for j, h in enumerate(self.humanPopulation):
+            """
+            To implement: update the human population.
+            """
+
+        """
+        To implement: update the data/statistics e.g. infectedCount,
+                      deathCount, etc.
+        """
+        return self.infectedCount, self.deathCount
+
+
+class Mosquito:
+    def __init__(self, x, y, hungry):
+        """
+        Class to model the mosquitos. Each mosquito is initialized with a random
+        position on the grid. Mosquitos can start out hungry or not hungry.
+        All mosquitos are initialized infection free (this can be modified).
+        """
+        self.position = [x, y]
+        self.hungry = hungry
+        self.infected = False
+
+    def bite(self, human, humanInfectionProb, mosquitoInfectionProb):
+        """
+        Function that handles the biting. If the mosquito is infected and the
+        target human is susceptible, the human can be infected.
+        If the mosquito is not infected and the target human is infected, the
+        mosquito can be infected.
+        After a mosquito bites it is no longer hungry.
+        """
+        if self.infected and human.state == 'S':
+            if np.random.uniform() <= humanInfectionProb:
+                human.state = 'I'
+        elif not self.infected and human.state == 'I':
+            if np.random.uniform() <= mosquitoInfectionProb:
+                self.infected = True
+        self.hungry = False
+
+    def move(self, height, width):
+        """
+        Moves the mosquito one step in a random direction.
+        """
+        deltaX = np.random.randint(-1, 2)
+        deltaY = np.random.randint(-1, 2)
+        """
+        The mosquitos may not leave the grid. There are two options:
+                      - fixed boundaries: if the mosquito wants to move off the
+                        grid choose a new valid move.
+                      - periodic boundaries: implement a wrap around i.e. if
+                        y+deltaY > ymax -> y = 0. This is the option currently implemented.
+        """
+        self.position[0] = (self.position[0] + deltaX) % width
+        self.position[1] = (self.position[1] + deltaY) % height
+
+
+class Human:
+    def __init__(self, x, y, state):
+        """
+        Class to model the humans. Each human is initialized with a random
+        position on the grid. Humans can start out susceptible or infected
+        (or immune).
+        """
+        self.position = [x, y]
+        self.state = state
+
+
+if __name__ == '__main__':
+    """
+    Simulation parameters
+    """
+    fileName = 'simulation'
+    timeSteps = 100
+    t = 0
+    plotData = False
+    """
+    Run a simulation for an indicated number of timesteps.
+    """
+    file = open(fileName + '.csv', 'w')
+    sim = Model()
+    vis = malaria_visualize.Visualization(sim.height, sim.width)
+    print('Starting simulation')
+    while t < timeSteps:
+        [d1, d2] = sim.update()  # Catch the data
+        line = str(t) + ',' + str(d1) + ',' + str(d2) + '\n'  # Separate the data with commas
+        file.write(line)  # Write the data to a .csv file
+        vis.update(t, sim.mosquitoPopulation, sim.humanPopulation)
+        t += 1
+    file.close()
+    vis.persist()
+
+    if plotData:
+        """
+        Make a plot by from the stored simulation data.
+        """
+        data = np.loadtxt(fileName+'.csv', delimiter=',')
+        time = data[:, 0]
+        infectedCount = data[:, 1]
+        deathCount = data[:, 2]
+        plt.figure()
+        plt.plot(time, infectedCount, label='infected')
+        plt.plot(time, deathCount, label='deaths')
+        plt.legend()
+        plt.show()
